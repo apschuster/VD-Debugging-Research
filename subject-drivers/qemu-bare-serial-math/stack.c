8acd1cd61e9b71244941a79793eeedfb14fa3ecc
Kyle <KRNelson4427@gmail.com>
2018-09-26 15:08:56 -0400

    The beginnings of the stack related functions are in this update.

--- subject-drivers/qemu-bare-serial-math/stack.c
+++ subject-drivers/qemu-bare-serial-math/stack.c
@@ -3,6 +3,7 @@
 /* Register name faking - works in collusion with the linker.  */
 register char * stack_ptr asm ("sp");
 //an implementation of _sbrk to enable newlib's malloc
+//THIS WILL MOST LIKELY NEED MORE TWEEKING
 caddr_t __attribute__((weak))
 _sbrk (int incr)
 {
@@ -43,25 +44,40 @@ string_stack* create()
 	string_stack* result = (string_stack*) malloc(sizeof(string_stack));
 	//assign enough memory to the stack initially for 10 strings
 	result->stack = (char**) malloc(sizeof(char*) * 10);
-	result->size = 10;
-	result->stack_pointer = 0;//point to bottom of the stack
+	result->size = 10;//we have space for 10 entries
+	result->stack_pointer = -1;//the stack is empty
+	return result;
 }
 
 //the destructor for the stack
 void destroy(string_stack* self)
 {
-
+	//go through the stack and deallocate all entries
+	for(int c = 0; c < self->size; c++)
+	{
+		//note that stack[c] may be a nullptr
+		//if it hasn't been allocated
+		free(self->stack[c]);
+	}
+	free(self->stack);//deallocate the stack itself
+	free(self);//deallocate the whole string_stack structure
 }
 
 //replace the existing stack with a larger one
 void resize(string_stack* self)
 {
-
+	//by default we will increase the size of the stack by 10
+	self->size += 10;
+	//stack should have the same contents, but be 10 entries larger
+	self->stack = (char**) realloc(self->stack, sizeof(char*)*(self->size));
 }
 
-//NOTE FOR THE FOLLOWING FUNCTIONS: 
-//the item pointed to may, or may not have been initialized
-//as such, these must account for both eventualities
+/*NOTE FOR THE FOLLOWING FUNCTIONS: 
+* the item pointed to may, or may not have been initialized
+* as such, these must account for both eventualities
+* ALSO: all entries are dynamically allocated.
+* As such, the pointers returned by these functions must be deallocated
+*/
 
 //set stack_pointer as one less, return previous top string
 char* pop(string_stack* self)
@@ -78,7 +94,14 @@ char* top(string_stack* self)
 //iterate stack_pointer, insert entry at this location
 void push(string_stack* self, char* entry)
 {
-
+	self->stack_pointer++;
+	//determine the size of the entry by iterating until the end of entry
+	int entry_size;
+	for(entry_size = 1; entry[entry_size] != '\00'; entry_size++) {};
+	//now we allocate enough space to the top of the stack for entry
+	self->stack[self->stack_pointer] = (char*)malloc(entry_size);
+	//now we copy entry into the top of the stack
+	strcpy(self->stack[self->stack_pointer], entry);
 }
 
 //return 0 if stack is not full, return nonzero if it is full
